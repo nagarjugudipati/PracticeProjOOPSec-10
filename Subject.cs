f51791a1eac46d25ef6768b887b75294bae3804e
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-08-02 21:36:16 +0530

    OOPPracticeProjectsec10

---
+++ Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppTeacherStudentOOPProject
+
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+        public string SubjectCode { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
---
+++ TeacherwithSubjects.cs
@@ -0,0 +1,21 @@
+﻿using ConAppTeacherStudentOOPProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppTeacherStudentOOPProject
+{
+    public class TeacherWithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherWithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+
+}
